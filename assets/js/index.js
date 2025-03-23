6a65f49a9c0c6fc521bb4629902cbf0b134f0d70
ghardinpeach <gary.a.peach@gmail.com>
2025-03-23 05:50:59 -0400

    button fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 import { PhotoViewer } from './PhotoViewer.js';
 
 const photoViewer = new PhotoViewer({
-    mainImage: document.querySelector('.photo-carousel'),
+    mainImage: document.querySelector('.photo-carousel, .photo-carousel-landscape'),
     thumbnails: document.querySelectorAll('.photo-thumbnail'),
 });
 
