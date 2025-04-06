09a00ce0eac2d70b2fd75cf2188b5c593ad9475b
ghardinpeach <gary.a.peach@gmail.com>
2025-04-05 23:21:27 -0400

    multipart upload s3 more

--- src/Service/CloudStorageFactory.php
+++ src/Service/CloudStorageFactory.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use Aws\S3\S3Client;
 use Spatie\Dropbox\Client as DropboxClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Psr\Log\LoggerInterface;
 
 class CloudStorageFactory
 {
@@ -14,6 +15,7 @@ class CloudStorageFactory
         private string $dropboxClientSecret,
         private string $dropboxRefreshToken,
         private string $awsS3Bucket,
+        private LoggerInterface $logger,
         private S3Client $s3,
         private HttpClientInterface $httpClient
     ) {}
@@ -21,7 +23,7 @@ class CloudStorageFactory
     public function getStorage(): CloudStorageInterface
     {
         return match ($this->cloudStorageDriver) {
-            's3' => new S3StorageService($this->s3, $this->awsS3Bucket),
+            's3' => new S3StorageService($this->s3, $this->awsS3Bucket, $this->logger),
             'dropbox' => new DropboxStorageService($this->getFreshDropboxClient()),
             default => throw new \RuntimeException("Unsupported cloud driver: {$this->cloudStorageDriver}"),
         };
