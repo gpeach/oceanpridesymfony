3222a003a8dc24963ed3e7ed8132f1f7e3eb19ea
ghardinpeach <gary.a.peach@gmail.com>
2025-04-30 01:39:30 -0400

    missing migrations

---
+++ migrations/Version20250430023953.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250430023953 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE gallery_image ADD provider VARCHAR(20) NOT NULL, ADD external_id VARCHAR(255) DEFAULT NULL, ADD external_url VARCHAR(255) DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE gallery_image DROP provider, DROP external_id, DROP external_url
+        SQL);
+    }
+}
