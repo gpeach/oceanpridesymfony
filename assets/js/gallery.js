df448a712368718bfd1fbe44e44a6917a3cf62a3
ghardinpeach <gary.a.peach@gmail.com>
2025-05-21 15:54:03 -0400

    set video quality to auto everywhere, more dead code cleanup

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -248,7 +248,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // External embed path
                 const src = provider === 'youtube'
                     ? `https://www.youtube.com/embed/${extId}?autoplay=1&rel=0`
-                    : `https://player.vimeo.com/video/${extId}?autoplay=1&quality=4k`;
+                    : `https://player.vimeo.com/video/${extId}?autoplay=1`;
 
                 const ifr = document.createElement('iframe');
                 ifr.src             = src;
