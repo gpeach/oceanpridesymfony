7052cbd72b2ebfcd8aa8251a7808f2c14f4bbc8d
ghardinpeach <gary.a.peach@gmail.com>
2025-05-24 23:53:41 -0400

    admin links

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class AdminController extends AbstractController
+{
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
